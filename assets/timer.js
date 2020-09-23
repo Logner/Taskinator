0e57365eb342b4a4612f2a09f7a3c8698aa4ff3a
KP <kumash123@gmail.com>
2020-09-22 22:52:08 -0400

    Save-task functionality v1

---
+++ assets/timer.js
@@ -0,0 +1,23 @@
+var counter = 10
+var countdown = function() {
+    console.log(counter);
+    counter --;
+    if (counter===0){
+        console.log("blastoff");
+
+        // Clears interval of the startCoundown variable
+        clearInterval(startCountdown)
+    };
+};
+// setInterval (func, milliseconds)
+// why does adding brackets to countdown only trigger the function once.
+var startCountdown = setInterval(countdown, 1000);
+
+// setTimeOut counter
+var sayHello = function(){
+    console.log("Hello There");
+};
+
+var timedGreeting = setTimeout(sayHello, 2000);
+
+// clearTimeout(timedGreeting);
