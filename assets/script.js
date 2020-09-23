d03d86a88e2dc0e3bf70bb08cdd457e5f0f81719
KP <kumash123@gmail.com>
2020-09-23 18:41:17 -0400

    added drag & drop functionality

--- assets/script.js
+++ assets/script.js
@@ -55,6 +55,7 @@ var createTaskEl = function(taskDataObj) {
   
     // add task id as a custom attribute
     listItemEl.setAttribute("data-task-id", taskIdCounter);
+    listItemEl.setAttribute("draggable", 'true');
   
     var taskInfoEl = document.createElement("div");
     taskInfoEl.className = "task-info";
@@ -180,6 +181,54 @@ var taskFormHandler = function(event) {
 
   };
 
+  var dragTaskHandler = function(event) {
+    var taskId = event.target.getAttribute('data-task-id');
+    console.log("Task ID:", taskId);
+
+    event.dataTransfer.setData("text/plain", taskId);
+  };
+
+  // how does this piece of code change target event???
+  // without this drop task handler doesnt trigger?
+  // prevent default acts as some sort of a gatekeeper?
+  var dropZoneDragHandler = function(event) {
+    var taskListEl = event.target.closest(".task-list");
+    if (taskListEl) {
+      event.preventDefault();
+    }
+  };
+
+  var dropTaskHandler = function(event) {
+    var id = event.dataTransfer.getData("text/plain");
+    console.log("drop Event Target:", event.target, event.dataTransfer, id);
+
+    // Storing the dropped element id
+    var draggableElement = document.querySelector("[data-task-id='" + id + "']");
+
+    // Getting the closest drop-zone id
+    var dropZoneEl = event.target.closest(".task-list");
+    var statusType = dropZoneEl.id;
+
+    // set status of task based on dropZone id
+    var statusSelectEl = draggableElement.querySelector("select[name='status-change']");
+
+    if (statusType === "tasks-to-do") {
+      statusSelectEl.selectedIndex = 0;
+    } 
+    else if (statusType === "tasks-in-progress") {
+      statusSelectEl.selectedIndex = 1;
+    } 
+    else if (statusType === "tasks-completed") {
+      statusSelectEl.selectedIndex = 2;
+    }
+
+    dropZoneEl.appendChild(draggableElement);
+
+  }
+
   formEl.addEventListener("submit", taskFormHandler);
   pageContentEl.addEventListener('click', taskButtonHandler);
   pageContentEl.addEventListener("change", taskStatusChangeHandler);
+  pageContentEl.addEventListener("dragstart", dragTaskHandler);
+  pageContentEl.addEventListener("dragover", dropZoneDragHandler);
+  pageContentEl.addEventListener("drop", dropTaskHandler);
